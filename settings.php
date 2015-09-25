806f431a4cb65502e729e45feb10dce292934ddc
Troy Williams <maxorsomeness@gmail.com>
2015-09-25 19:36:02 +1200

    Clean up to Moodle CS, added default role and unenrol action.

--- settings.php
+++ settings.php
@@ -1,16 +1,47 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
- * @package     Arlo Moodle Integration
- * @subpackage  enrol_arlo
- * @author 		Corey Davis
+ * Arlo enrolment plugin settings and presets.
+ *
+ * @package     enrol_arlo
+ * @author      Troy Williams
+ * @author      Corey Davis
  * @copyright   2015 LearningWorks Ltd <http://www.learningworks.co.nz>
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-// defined('MOODLE_INTERNAL') || die();
+defined('MOODLE_INTERNAL') || die();
 
-// if ($ADMIN->fulltree) {
+if ($ADMIN->fulltree) {
 
-//     //--- heading ---
-//     $settings->add(new admin_setting_heading('enrol_arlo_settings', '', get_string('pluginname_desc', 'enrol_arlo')));
-// }
+    $settings->add(new admin_setting_heading('enrol_arlo_settings', '', get_string('pluginname_desc', 'enrol_arlo')));
+
+    if (!during_initial_install()) {
+        $options = get_default_enrol_roles(context_system::instance());
+        $student = get_archetype_roles('student');
+        $student = reset($student);
+        $settings->add(new admin_setting_configselect('enrol_arlo/roleid',
+            get_string('defaultrole', 'role'), '', $student->id, $options));
+
+        $options = array(
+            ENROL_EXT_REMOVED_UNENROL        => get_string('extremovedunenrol', 'enrol'),
+            ENROL_EXT_REMOVED_SUSPENDNOROLES => get_string('extremovedsuspendnoroles', 'enrol'));
+        $settings->add(new admin_setting_configselect('enrol_arlo/unenrolaction',
+            get_string('extremovedaction', 'enrol'),
+            get_string('extremovedaction_help', 'enrol'), ENROL_EXT_REMOVED_UNENROL, $options));
+    }
+}
