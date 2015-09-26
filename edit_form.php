cc3168143f18d9b65e5cdebb35e13265ecaad679
Troy Williams <maxorsomeness@gmail.com>
2015-09-26 16:03:54 +1200

    Rebuilt the skeleton of the form definition.

--- edit_form.php
+++ edit_form.php
@@ -1,53 +1,94 @@
 <?php
+//
+// This file is part of Moodle - http://moodle.org/
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
- * @package     Arlo Moodle Integration
- * @subpackage  enrol_arlo
- * @author      Corey Davis
- * @copyright   2015 LearningWorks Ltd <http://www.learningworks.co.nz>
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * Adds new instance of enrol_arlo to specified course
+ * or edits current instance.
+ *
+ * @author    Troy Williams
+ * @author    Corey Davis
+ * @package   local_arlo {@link https://docs.moodle.org/dev/Frankenstyle}
+ * @copyright 2015 LearningWorks Ltd {@link http://www.learningworks.co.nz}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG->libdir.'/formslib.php');
+require_once($CFG->dirroot.'/cohort/lib.php');
 
-//$arloCore = new local_arlo_core();
 class enrol_arlo_edit_form extends moodleform {
-
-    function definition() {
+    public function definition() {
         $mform = $this->_form;
 
-        list($instance, $plugin, $context) = $this->_customdata;
-        //$plugin = enrol_get_plugin('arlo');
+        list($instance, $plugin, $course) = $this->_customdata;
+        $context = context_course::instance($course->id);
 
-        $mform->addElement('header', 'header', get_string('pluginname', 'enrol_arlo'));
-        $templates = $plugin->getTemplateCodes($instance);
-        if ($templates != null){
-            $mform->addElement('select', 'customchar1', get_string('customchar1', 'enrol_arlo'), $templates);
-            $mform->addHelpButton('customchar1', 'customchar1', 'enrol_arlo');
-            $mform->setType('customchar1', PARAM_TEXT);
-            $mform->setDefault('customchar1', $plugin->get_config('customchar1'));
+        $enrol = enrol_get_plugin('arlo');
 
-            $mform->addElement('hidden', 'courseid');
-            $mform->setType('courseid', PARAM_INT);
-
-            if (enrol_accessing_via_instance($instance)) {
-                $mform->addElement('static', 'selfwarn', get_string('instanceeditselfwarning', 'core_enrol'), get_string('instanceeditselfwarningtext', 'core_enrol'));
-            }
-
-            $this->add_action_buttons(true, ($instance->id ? null : get_string('addinstance', 'enrol')));
-        }else{
-            $mform->addElement('static', 'selfwarn', get_string('instanceeditselfwarning', 'core_enrol'), get_string('notemplatesavali', 'enrol_arlo'));
+        $groups = array(0 => get_string('none'));
+        if (has_capability('moodle/course:managegroups', $context)) {
+            $groups[ARLO_CREATE_GROUP] = get_string('creategroup', 'enrol_arlo');
         }
-        
+
+        foreach (groups_get_all_groups($course->id) as $group) {
+            $groups[$group->id] = format_string($group->name, true, array('context' => $context));
+        }
+
+        $options = array(ENROL_INSTANCE_ENABLED  => get_string('yes'),
+            ENROL_INSTANCE_DISABLED => get_string('no'));
+        $mform->addElement('select', 'status', get_string('status', 'enrol_arlo'), $options);
+
+        // @TODO this is need to build selector.
+        if ($instance->id) {
+
+        } else {
+            $events = arlo_get_events($instance);
+            $onlineactivities = arlo_get_online_activities($instance);
+        }
+
+        $mform->addElement('select', 'customint2', get_string('addgroup', 'enrol_arlo'), $groups);
+
+        $mform->addElement('advcheckbox', 'customint8', get_string('sendcoursewelcomemessage', 'enrol_self'));
+        $mform->addHelpButton('customint8', 'sendcoursewelcomemessage', 'enrol_self');
+        $mform->setDefault('customint8', 1);
+
+        $mform->addElement('textarea', 'customtext1',
+            get_string('customwelcomemessage', 'enrol_arlo'),
+            array('cols'=>'60', 'rows'=>'8'));
+        $mform->addHelpButton('customtext1', 'customwelcomemessage', 'enrol_arlo');
+
+        $mform->addElement('hidden', 'id');
+        $mform->setType('id', PARAM_INT);
+        $mform->addElement('hidden', 'courseid');
+        $mform->setType('courseid', PARAM_INT);
+
+        if (enrol_accessing_via_instance($instance)) {
+            $mform->addElement('static', 'selfwarn',
+                get_string('instanceeditselfwarning', 'core_enrol'),
+                get_string('instanceeditselfwarningtext', 'core_enrol'));
+        }
+
+        $this->add_action_buttons(true, ($instance->id ? null : get_string('addinstance', 'enrol')));
 
         $this->set_data($instance);
     }
-
-    function validation($data, $files) {
+    public function validation($data, $files) {
         global $DB;
         $errors = parent::validation($data, $files);
-
         return $errors;
     }
 }
