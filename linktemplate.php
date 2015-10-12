4102e7b11991a7af84e87ccadf3c3a0752005c5c
Troy Williams <maxorsomeness@gmail.com>
2015-10-12 21:08:15 +1300

    Add link template pages.

---
+++ linktemplate.php
@@ -0,0 +1,75 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *
+ * @author      Troy Williams
+ * @package     enrol_arlo {@link https://docs.moodle.org/dev/Frankenstyle}
+ * @copyright   2015 LearningWorks Ltd {@link http://www.learningworks.co.nz}
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require('../../config.php');
+require_once($CFG->dirroot . '/enrol/arlo/linktemplate_form.php');
+require_once($CFG->dirroot . '/enrol/arlo/locallib.php');
+
+$courseid   = required_param('courseid', PARAM_INT);
+$linkid     = optional_param('id', 0, PARAM_INT);
+
+$course = $DB->get_record('course', array('id' => $courseid), '*', MUST_EXIST);
+$context = context_course::instance($course->id, MUST_EXIST);
+
+require_login($course);
+require_capability('enrol/arlo:config', $context);
+
+$returnurl = new moodle_url('/enrol/instances.php', array('id' => $course->id));
+$PAGE->set_url('/enrol/arlo/linktemplate.php', array('courseid' => $course->id));
+
+$link = $DB->get_record('local_arlo_course', array('courseid' => $course->id));
+if (! $link) {
+    $link = new stdClass();
+    $link->id         = 0;
+    $link->courseid   = $course->id;
+}
+
+$mform = new enrol_arlo_linktemplate_form(null, array($link, $course));
+if ($mform->is_cancelled()) {
+    redirect($returnurl);
+} else if ($data = $mform->get_data()) {
+    if ($data->id) {
+        if (isset($data->submitbuttonremove)) {
+            $DB->delete_records('local_arlo_course', array('courseid' => $data->courseid));
+        }
+    } else {
+        $link = new \stdClass();
+        $link->courseid = $data->courseid;
+        $link->arloguid = $data->template;
+        $link->modified = time();
+        $link->id = $DB->insert_record('local_arlo_course', $link);
+    }
+    $trace = new null_progress_trace();
+    //enrol_arlo_sync($trace, $course->id);
+    $trace->finished();
+    redirect($returnurl);
+}
+$PAGE->set_title(get_string('linktemplatetocourse', 'enrol_arlo'));
+$PAGE->set_heading($course->fullname);
+
+echo $OUTPUT->header();
+echo $OUTPUT->heading(get_string('linktemplatetocourse', 'enrol_arlo'));
+$mform->display();
+echo $OUTPUT->footer();
+
---
+++ linktemplate_form.php
@@ -0,0 +1,71 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @author      Troy Williams
+ * @package     enrol_arlo {@link https://docs.moodle.org/dev/Frankenstyle}
+ * @copyright   2015 LearningWorks Ltd {@link http://www.learningworks.co.nz}
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->libdir  . '/formslib.php');
+require_once($CFG->dirroot . '/enrol/arlo/lib.php');
+
+class enrol_arlo_linktemplate_form extends moodleform{
+    public function definition() {
+        global $DB;
+        $mform = $this->_form;
+
+        list($link, $course) = $this->_customdata;
+
+        $templates = array();
+        // Build online activity options group.
+        foreach (\local_arlo\arlo::get_active_templates() as $key => $template) {
+            $templates[$key] = $template->code . ' ' . $template->name;
+        }
+        if ($link->id) {
+            $mform->addElement('select', 'template', get_string('template', 'enrol_arlo'), $templates);
+            $mform->setConstant('template', $link->arloguid);
+            $mform->hardFreeze('template', $link->arloguid);
+        } else {
+            $mform->addElement('select', 'template', get_string('template', 'enrol_arlo'), $templates);
+        }
+        $mform->addElement('hidden', 'courseid');
+        $mform->setType('courseid', PARAM_INT);
+        $mform->addElement('hidden', 'id', null);
+        $mform->setType('id', PARAM_INT);
+        if ($link->id) {
+            $this->add_remove_button();
+        } else {
+            $this->add_action_buttons(true);
+        }
+        $this->set_data($link);
+    }
+    /**
+     * Adds buttons on create new method form
+     */
+    protected function add_remove_button() {
+        $mform = $this->_form;
+        $buttonarray = array();
+        $buttonarray[0] = $mform->createElement('submit', 'submitbuttonremove', get_string('remove', 'enrol_arlo'));
+        $buttonarray[1] = $mform->createElement('cancel');
+        $mform->addGroup($buttonarray, 'buttonar', '', array(' '), false);
+        $mform->closeHeaderBefore('buttonar');
+    }
+}
