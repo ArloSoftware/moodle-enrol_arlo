abc8d7f859c18b5f984c0a00b608fb7e0f0db51f
Troy Williams <maxorsomeness@gmail.com>
2015-10-13 19:57:23 +1300

    Code for syncing not yet enrolled users and syncing groups.

--- locallib.php
+++ locallib.php
@@ -32,7 +32,7 @@
  */
 function enrol_arlo_sync(progress_trace $trace, $courseid = null) {
     global $CFG, $DB;
-    require_once("$CFG->dirroot/group/lib.php");
+    require_once($CFG->dirroot . '/group/lib.php');
 
     // Purge all roles if cohort sync disabled, those can be recreated later here by cron or CLI.
     if (!enrol_is_enabled('arlo')) {
@@ -45,11 +45,58 @@ function enrol_arlo_sync(progress_trace $trace, $courseid = null) {
     core_php_time_limit::raise();
     raise_memory_limit(MEMORY_HUGE);
 
-    //$trace->output('Starting user enrolment synchronisation...');
+    $trace->output('Starting user enrolment synchronisation...');
 
-    $allroles = get_all_roles();
-    $instances = array(); //cache
+    $instances = array(); // Cache.
 
     $plugin = enrol_get_plugin('arlo');
-    //$unenrolaction = $plugin->get_config('unenrolaction', ENROL_EXT_REMOVED_UNENROL);
+    $unenrolaction = $plugin->get_config('unenrolaction', ENROL_EXT_REMOVED_UNENROL);
+
+    // Iterate through all not enrolled yet users.
+    $onecourse = $courseid ? "AND e.courseid = :courseid" : "";
+    $sql = "SELECT u.id AS userid, e.id AS enrolid, ue.status
+              FROM {user} u
+              JOIN {user_info_data} AS uid
+                ON (uid.userid = u.id AND u.deleted = 0)
+              JOIN {user_info_field} AS uif
+                ON (uid.fieldid = uif.id AND uif.shortname = 'arloguid')
+              JOIN {local_arlo_registrations} r ON r.contactguid = uid.data
+              JOIN {enrol} e ON ((e.customchar3 = r.eventguid OR e.customchar3 = r.onlineactivityguid)
+               AND e.enrol = 'arlo' $onecourse)
+         LEFT JOIN {user_enrolments} ue ON (ue.enrolid = e.id AND ue.userid = u.id)
+             WHERE ue.id IS NULL OR ue.status = :suspended";
+
+    $params = array();
+    $params['courseid'] = $courseid;
+    $params['suspended'] = ENROL_USER_SUSPENDED;
+    $rs = $DB->get_recordset_sql($sql, $params);
+    foreach($rs as $ue) {
+        if (!isset($instances[$ue->enrolid])) {
+            $instances[$ue->enrolid] = $DB->get_record('enrol', array('id' => $ue->enrolid));
+        }
+        $instance = $instances[$ue->enrolid];
+        if ($ue->status == ENROL_USER_SUSPENDED) {
+            //$plugin->update_user_enrol($instance, $ue->userid, ENROL_USER_ACTIVE);
+            //$trace->output("unsuspending: $ue->userid ==> $instance->courseid via arlo $instance->customint1", 1);
+            $trace->output('suspended', 1);
+        } else {
+            $plugin->enrol_user($instance, $ue->userid);
+            $trace->output("enrolling: $ue->userid > $instance->courseid via Arlo", 1);
+        }
+    }
+    $rs->close();
+
+
+    // Sync groups.
+    $affectedusers = groups_sync_with_enrolment('arlo', $courseid);
+    foreach ($affectedusers['removed'] as $gm) {
+        $trace->output("removing user from group: $gm->userid > $gm->courseid - $gm->groupname", 1);
+    }
+    foreach ($affectedusers['added'] as $ue) {
+        $trace->output("adding user to group: $ue->userid > $ue->courseid - $ue->groupname", 1);
+    }
+
+    $trace->output('...user enrolment synchronisation finished.');
+
+    return 0;
 }
