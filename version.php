7f2a8b060219f57b668c0e6af82f8ad996b93497
Troy Williams <maxorsomeness@gmail.com>
2015-10-15 10:51:11 +1300

    Cleaned up version file.

--- version.php
+++ version.php
@@ -1,20 +1,34 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
- * @package     Arlo Moodle Integration
- * @subpackage  enrol_arlo
- * @author 		Corey Davis
- * @copyright   2015 LearningWorks Ltd <http://www.learningworks.co.nz>
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *  Version information
+ *
+ * @author    Troy Williams
+ * @author    Corey Davis
+ * @package   local_arlo {@link https://docs.moodle.org/dev/Frankenstyle}
+ * @copyright 2015 LearningWorks Ltd {@link http://www.learningworks.co.nz}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015070901;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2014050800;        // Requires this Moodle version
+$plugin->version   = 2015070901;
+$plugin->requires  = 2014051200;        // See http://docs.moodle.org/dev/Moodle_Version
 $plugin->component = 'enrol_arlo';      // Full name of the plugin (used for diagnostics)
-$plugin->maturity = MATURITY_STABLE;  // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
-$plugin->release  = '1.0.0';
-$plugin->dependencies = array(
-    'local_arlo' => 2015070901,
-);
-$plugin->cron      = 0;
+$plugin->release   = '2.9.0';           // Human-friendly version name
+$plugin->maturity  = MATURITY_BETA;     // This version's maturity level
+$plugin->dependencies = array('enrol_local' => ANY_VERSION); //@TODO change once out of dev.
