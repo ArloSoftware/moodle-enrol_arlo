640edd1c54ab6f7567ffaef6a8fa4b983ff5c306
Troy Williams <troy.williams@learningworks.co.nz>
2017-01-11 14:24:41 +1300

    Release 3.0.0

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2015101504;
-$plugin->requires  = 2014051200;        // See http://docs.moodle.org/dev/Moodle_Version
+$plugin->requires  = 2015111600;        // See http://docs.moodle.org/dev/Moodle_Version
 $plugin->component = 'enrol_arlo';      // Full name of the plugin (used for diagnostics).
-$plugin->release   = '2.9.0';           // Human-friendly version name.
-$plugin->maturity  = MATURITY_BETA;     // This version's maturity level.
+$plugin->release   = '3.0.0';           // Human-friendly version name.
+$plugin->maturity  = MATURITY_STABLE;   // This version's maturity level.
 $plugin->dependencies = array('local_arlo' => 2015101504);
