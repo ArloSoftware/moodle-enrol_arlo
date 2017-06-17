765b1ed47348c1e4c1968479dd7d072f570f99dd
Troy Williams <troy.williams@learningworks.co.nz>
2017-06-17 13:42:13 +1200

    Set default configuration settings.

--- db/install.php
+++ db/install.php
@@ -24,9 +24,21 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+use enrol_arlo\plugin_config;
+
 defined('MOODLE_INTERNAL') || die('Direct access to this script is forbidden.');
 
 function xmldb_enrol_arlo_install() {
     global $CFG, $DB;
+
+    plugin_config::set('apistatus', plugin_config::get_default('apistatus'));
+    plugin_config::set('authplugin', plugin_config::get_default('authplugin'));
+    plugin_config::set('matchuseraccountsby', plugin_config::get_default('matchuseraccountsby'));
+    plugin_config::set('unenrolaction', plugin_config::get_default('unenrolaction'));
+    plugin_config::set('expiredaction', plugin_config::get_default('expiredaction'));
+    plugin_config::set('pushonlineactivityresults', plugin_config::get_default('pushonlineactivityresults'));
+    plugin_config::set('pusheventresults', plugin_config::get_default('pusheventresults'));
+    plugin_config::set('alertsiteadmins', plugin_config::get_default('alertsiteadmins'));
+
     return true;
 }
