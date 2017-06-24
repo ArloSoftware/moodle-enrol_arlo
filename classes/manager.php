ee1129fa82065fae2adb385d2b8ef9b0fa026101
Troy Williams <troy.williams@learningworks.co.nz>
2017-06-24 16:52:12 +1200

    Move check response into parse response. Save templates to database.

--- classes/manager.php
+++ classes/manager.php
@@ -6,7 +6,10 @@ use enrol_arlo\Arlo\AuthAPI\Client;
 use enrol_arlo\Arlo\AuthAPI\RequestUri;
 use enrol_arlo\Arlo\AuthAPI\Filter;
 use enrol_arlo\Arlo\AuthAPI\XmlDeserializer;
+use enrol_arlo\Arlo\AuthAPI\Resource\AbstractCollection;
+use enrol_arlo\Arlo\AuthAPI\Resource\AbstractResource;
 use enrol_arlo\Arlo\AuthAPI\Resource\ApiException;
+use enrol_arlo\utility\date;
 use GuzzleHttp\Psr7\Response;
 
 class manager {
@@ -28,45 +31,138 @@ class manager {
         $this->apipassword = $apipassword;
     }
 
+    private function get_lastestmodified_field($table) {
+        global $DB;
+        $sql = "SELECT MAX(t.sourcemodified) AS latestmodified 
+                  FROM {{$table}} t
+                 WHERE t.platform = :platform";
+        return (int) $DB->get_field_sql($sql, array('platform' => $this->platform));
+    }
+
 
     public function get_templates() {
-
+        global $DB;
         // Can run? Check API status.
         //self::check_apistatus();
-
-        $timestart = microtime();
-
         try {
-            $client = new Client($this->platform, $this->apiusername, $this->apipassword);
-            // Setup RequestUri for getting Templates.
-            $requesturi = new RequestUri();
-            $requesturi->setHost($this->platform);
-            $requesturi->setResourcePath('eventtemplates/');
-            $requesturi->setPagingTop(5);
+
+            //$cnt = 0;
+            $hasnext = true; // Initialise to true for first fetch.
+            while ($hasnext) {
+                $hasnext = false;
+                $latestmodified = self::get_lastestmodified_field('enrol_arlo_template');
+                // Setup RequestUri for getting Templates.
+                $requesturi = new RequestUri();
+                $requesturi->setHost($this->platform);
+                $requesturi->setOrderBy('LastModifiedDateTime ASC'); // Important.
+                $requesturi->setResourcePath('eventtemplates/');
+                $requesturi->addExpand('EventTemplate');
+                $createdfilter = Filter::create()
+                    ->setResourceField('CreatedDateTime')
+                    ->setOperator('gt')
+                    ->setDateValue(date::create($latestmodified));
+                $modifiedfilter = Filter::create()
+                    ->setResourceField('LastModifiedDateTime')
+                    ->setOperator('gt')
+                    ->setDateValue(date::create($latestmodified));
+                $requesturi->addFilter($createdfilter);
+                $requesturi->addFilter($modifiedfilter);
+
+                $client = new Client($this->platform, $this->apiusername, $this->apipassword);
+                $timestart = microtime();
+                $response = $client->request('GET', $requesturi);
+
+                $templates = self::parse_response($response);
+                if ($templates) {
+                    if ($templates instanceof AbstractCollection) {
+                        foreach ($templates as $template) {
+                            $record = new \stdClass();
+                            $record->platform = $this->platform;
+                            $record->sourceid = $template->TemplateID;
+                            $record->sourceguid = $template->UniqueIdentifier;
+                            $record->name = $template->Name;
+                            $record->code = $template->Code;
+                            $record->sourcestatus = $template->Status;
+                            $record->sourcecreated = date::create($template->CreatedDateTime)->getTimestamp();
+                            $record->sourcemodified = date::create($template->LastModifiedDateTime)->getTimestamp();
+
+                            $params = array(
+                                'platform' => $this->platform,
+                                'sourceid' => $template->TemplateID,
+                                'sourceguid' => $template->UniqueIdentifier
+                            );
+                            $record->id = $DB->get_field('enrol_arlo_template', 'id', $params);
+                            if (empty($record->id)) {
+                                unset($record->id);
+                                $DB->insert_record('enrol_arlo_template', $record);
+                            } else {
+                                $DB->update_record('enrol_arlo_template', $record);
+                            }
+                        }
+                        $hasnext = (bool) $templates->hasNext();
+                    }
+                }
+            }
 
             $log = array(
                 'timelogged' => $timestart,
                 'platform' => $this->platform,
                 'uri' => (string) $requesturi
             );
-            $response = $client->request('GET', $requesturi);
-            $template = self::parse_response($response);
-            if (!$template) {
 
-            }
-
-            if (self::response_ok($response)) {
-
-            } else {
-
-            }
 
         } catch (\Exception $e) {
-            mtrace($e->getMessage());
+            print_object($e);
+            die;
         }
         $timefinish = microtime();
-        $difftime = microtime_diff($timestart, $timefinish);
-        mtrace("Execution took {$difftime} seconds");
+       // $difftime = microtime_diff($timestart, $timefinish);
+       // mtrace("Execution took {$difftime} seconds");
+    }
+
+    private function parse_response(Response $response) {
+        $status = (int) $response->getStatusCode();
+        plugin_config::set('apistatus', $status);
+        $this->lastresponsestatus = $status;
+
+        // Incorrect content-type.
+        $contenttype = $response->getHeaderLine('content-type');
+        if (strpos($contenttype, 'application/xml') === false) {
+            self::alert('error_incorrectcontenttype' , array('contenttype' => $contenttype));
+            return false;
+        }
+
+        $deserializer = new XmlDeserializer('\enrol_arlo\Arlo\AuthAPI\Resource\\');
+        $stream = $response->getBody();
+        $contents = $stream->getContents();
+        if ($stream->eof()) $stream->rewind(); // Rewind stream.
+        $resource = $deserializer->deserialize($contents);
+
+        // Handle HTTP Status errors.
+        if ($status >= 400 && $status < 599) {
+            $exceptioncode = '';
+            $exceptionmessage = '';
+            if ($resource instanceof ApiException) {
+                $exceptioncode = $resource->Code;
+                $exceptionmessage = $resource->Message;
+            }
+            // Custom 401 Unauthorized, 403 Forbidden messages.
+            if ($status == 401 || $status == 403) {
+                $params = array('url' => $CFG->wwwroot . '/admin/settings.php?section=enrolsettingsarlo');
+                self::alert('error_' . $status, $params);
+                return false;
+            } else {
+                $params = array(
+                    'status' => $status,
+                    'reason' => $response->getReasonPhrase(),
+                    'exceptioncode' => $exceptioncode,
+                    'exceptionmessage' => $exceptionmessage
+                );
+                self::alert('error_xxx', $params);
+                return false;
+            }
+        }
+        return $resource;
     }
 
     /**
@@ -86,55 +182,6 @@ class manager {
             $item->info = (string) $info;
         }
         $DB->insert_record('enrol_arlo_webservicelog', $item);
-    }
-
-
-    private function response_ok(Response $response) {
-        global $CFG;
-        $status = (int) $response->getStatusCode();
-        plugin_config::set('apistatus', $status);
-        // Incorrect content-type.
-        $contenttype = $response->getHeaderLine('content-type');
-        if (strpos($contenttype, 'application/xml') === false) {
-            self::alert('error_incorrectcontenttype' , array('contenttype' => $contenttype));
-            return false;
-        }
-        // Handle HTTP Status errors.
-        if ($status >= 400 && $status < 599) {
-            $exception = self::parse_response($response);
-            $exceptioncode = '';
-            $exceptionmessage = '';
-            if ($exception instanceof ApiException) {
-                $exceptioncode = $exception->Code;
-                $exceptionmessage = $exception->Message;
-            }
-            // Custom 401 Unauthorized, 403 Forbidden messages.
-            if ($status == 401 || $status == 403) {
-                $params = array('url' => $CFG->wwwroot . '/admin/settings.php?section=enrolsettingsarlo');
-                self::alert('error_' . $status, $params);
-                return false;
-            } else {
-                $params = array(
-                    'status' => $status,
-                    'reason' => $response->getReasonPhrase(),
-                    'exceptioncode' => $exceptioncode,
-                    'exceptionmessage' => $exceptionmessage
-                );
-                self::alert('error_xxx', $params);
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private function parse_response(Response $response) {
-        $deserializer = new XmlDeserializer('\enrol_arlo\Arlo\AuthAPI\Resource\\');
-        $stream = $response->getBody();
-        $body = $stream->getContents();
-        $this->lastresponsestatus = (int) $response->getStatusCode();
-        $this->lastresponsebody = $body;
-        if ($stream->eof()) $stream->rewind(); // Rewind stream.
-        return $deserializer->deserialize($body);
     }
 
     /**
