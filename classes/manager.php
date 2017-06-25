36f8ddee3bb245140bbacce0cc01bc9980e35a70
Troy Williams <troy.williams@learningworks.co.nz>
2017-06-25 15:19:32 +1200

    Tidy up get template and parse response.

--- classes/manager.php
+++ classes/manager.php
@@ -9,26 +9,32 @@ use enrol_arlo\Arlo\AuthAPI\XmlDeserializer;
 use enrol_arlo\Arlo\AuthAPI\Resource\AbstractCollection;
 use enrol_arlo\Arlo\AuthAPI\Resource\AbstractResource;
 use enrol_arlo\Arlo\AuthAPI\Resource\ApiException;
+use enrol_arlo\exception\client_exception;
+use enrol_arlo\exception\server_exception;
 use enrol_arlo\utility\date;
 use GuzzleHttp\Psr7\Response;
+
 
 class manager {
     private $platform;
     private $apiusername;
     private $apipassword;
-
-
-    private $lastresponsestatus;
-    private $lastresponsebody;
+    private $trace;
 
 
     public function __construct($platform = null,
                                 $apiusername = null,
-                                $apipassword = null) {
+                                $apipassword = null,
+                                \progress_trace $trace = null) {
 
         $this->platform = $platform;
         $this->apiusername = $apiusername;
         $this->apipassword = $apipassword;
+        if (is_null($trace)) {
+            $this->trace = new \null_progress_trace();
+        } else {
+            $this->trace = $trace;
+        }
     }
 
     private function get_lastestmodified_field($table) {
@@ -39,14 +45,19 @@ class manager {
         return (int) $DB->get_field_sql($sql, array('platform' => $this->platform));
     }
 
+    public static function update_api_status($status) {
+        if (!is_int($status)) {
+            throw new \Exception('API Status must integer.');
+        }
+        plugin_config::set('apistatus', $status);
+    }
 
+    // Can run? Check API status.
+    //self::check_apistatus();
     public function get_templates() {
         global $DB;
-        // Can run? Check API status.
-        //self::check_apistatus();
-        try {
 
-            //$cnt = 0;
+        try {
             $hasnext = true; // Initialise to true for first fetch.
             while ($hasnext) {
                 $hasnext = false;
@@ -64,16 +75,26 @@ class manager {
                 $modifiedfilter = Filter::create()
                     ->setResourceField('LastModifiedDateTime')
                     ->setOperator('gt')
-                    ->setDateValue(date::create($latestmodified));
+                    ->setDateValue(date::create($latestmodified),true);
                 $requesturi->addFilter($createdfilter);
                 $requesturi->addFilter($modifiedfilter);
-
+                // Get HTTP client.
                 $client = new Client($this->platform, $this->apiusername, $this->apipassword);
+                // Start the clock.
                 $timestart = microtime();
+                self::trace(sprintf('Fetching EventTemplates modified after: %s',
+                    date::create($latestmodified)->format(DATE_ISO8601)));
+                // Launch HTTP client request to API and get response.
                 $response = $client->request('GET', $requesturi);
+                // Set API status.
+                self::update_api_status((int) $response->getStatusCode());
+                // Log the request uri and response status.
+                //$logentry = self::log(time(), (string) $requesturi, $status);
 
                 $templates = self::parse_response($response);
-                if ($templates) {
+                if (!$templates) {
+                    self::trace("No new or updated resources found.");
+                } else {
                     if ($templates instanceof AbstractCollection) {
                         foreach ($templates as $template) {
                             $record = new \stdClass();
@@ -95,74 +116,88 @@ class manager {
                             if (empty($record->id)) {
                                 unset($record->id);
                                 $DB->insert_record('enrol_arlo_template', $record);
+                                self::trace(sprintf('Created: %s', $record->name));
                             } else {
                                 $DB->update_record('enrol_arlo_template', $record);
+                                self::trace(sprintf('Updated: %s', $record->name));
                             }
                         }
                         $hasnext = (bool) $templates->hasNext();
                     }
                 }
             }
-
-            $log = array(
-                'timelogged' => $timestart,
-                'platform' => $this->platform,
-                'uri' => (string) $requesturi
-            );
-
-
         } catch (\Exception $e) {
-            print_object($e);
-            die;
+            // TODO Any extra handling/logging?
+            if ($e instanceof client_exception) {
+                self::trace($e->getCode() . ' ' . $e->getMessage());
+                $identifier = $e->getStringidentifier();
+                $params = $e->getParameters();
+                self::alert($identifier, $params);
+            }
+            return false;
         }
         $timefinish = microtime();
-       // $difftime = microtime_diff($timestart, $timefinish);
-       // mtrace("Execution took {$difftime} seconds");
+        $difftime = microtime_diff($timestart, $timefinish);
+        self::trace("Execution took {$difftime} seconds");
+        return true;
     }
 
     private function parse_response(Response $response) {
+        global $CFG;
+        $resourceclass = false;
+        // Returned HTTP status, used for error checking.
         $status = (int) $response->getStatusCode();
-        plugin_config::set('apistatus', $status);
-        $this->lastresponsestatus = $status;
-
+        $reason = $response->getReasonPhrase();
         // Incorrect content-type.
         $contenttype = $response->getHeaderLine('content-type');
         if (strpos($contenttype, 'application/xml') === false) {
-            self::alert('error_incorrectcontenttype' , array('contenttype' => $contenttype));
-            return false;
+            throw new server_exception(
+                $reason,
+                $status,
+                'error_incorrectcontenttype',
+                array('contenttype' => $contenttype)
+                );
         }
-
+        // Deserialize response body.
         $deserializer = new XmlDeserializer('\enrol_arlo\Arlo\AuthAPI\Resource\\');
         $stream = $response->getBody();
         $contents = $stream->getContents();
         if ($stream->eof()) $stream->rewind(); // Rewind stream.
-        $resource = $deserializer->deserialize($contents);
-
-        // Handle HTTP Status errors.
-        if ($status >= 400 && $status < 599) {
-            $exceptioncode = '';
-            $exceptionmessage = '';
-            if ($resource instanceof ApiException) {
-                $exceptioncode = $resource->Code;
-                $exceptionmessage = $resource->Message;
-            }
+        $resourceclass = $deserializer->deserialize($contents);
+        // Get api exception information if available.
+        $apiexceptioninfo = array();
+        if ($resourceclass instanceof ApiException) {
+            $apiexceptioninfo['code'] = $resourceclass->Code;
+            $apiexceptioninfo['message'] = $resourceclass->Message;
+        }
+        // Client side.
+        if ($status >= 400 && $status < 499) {
+            $identifier = 'error_4xx';
+            $params = array();
             // Custom 401 Unauthorized, 403 Forbidden messages.
             if ($status == 401 || $status == 403) {
+                $identifier = 'error_' . $status;
                 $params = array('url' => $CFG->wwwroot . '/admin/settings.php?section=enrolsettingsarlo');
-                self::alert('error_' . $status, $params);
-                return false;
-            } else {
-                $params = array(
-                    'status' => $status,
-                    'reason' => $response->getReasonPhrase(),
-                    'exceptioncode' => $exceptioncode,
-                    'exceptionmessage' => $exceptionmessage
-                );
-                self::alert('error_xxx', $params);
-                return false;
             }
+            throw new client_exception(
+                $reason,
+                $status,
+                $identifier,
+                $params,
+                $apiexceptioninfo);
+        // Server side.
+        } else if ($status >= 500 && $status < 599) {
+            $identifier = 'error_5xx';
+            $params = array();
+            throw new server_exception(
+                $reason,
+                $status,
+                $identifier,
+                $params,
+                $apiexceptioninfo);
         }
-        return $resource;
+        // If everything went OK a resource class will be returned.
+        return $resourceclass;
     }
 
     /**
@@ -170,18 +205,21 @@ class manager {
      * @param $uri
      * @param $status
      * @param string $info
+     * @return \stdClass
      */
     private function log($timelogged, $uri, $status, $info = '') {
         global $DB;
 
         $item = new \stdClass();
         $item->timelogged = $timelogged;
+        $item->platform = $this->platform;
         $item->uri = $uri;
         $item->status = $status;
         if ($info != '') {
             $item->info = (string) $info;
         }
-        $DB->insert_record('enrol_arlo_webservicelog', $item);
+        $item->id = $DB->insert_record('enrol_arlo_webservicelog', $item);
+        return $item;
     }
 
     /**
@@ -229,5 +267,15 @@ class manager {
             $messagecopy->userto = $admin;
             message_send($messagecopy);
         }
+    }
+
+    /**
+     * Output a progress message.
+     *
+     * @param $message the message to output.
+     * @param int $depth indent depth for this message.
+     */
+    private function trace($message, $depth = 0) {
+        $this->trace->output($message, $depth);
     }
 }
