f56ab430b2c29a530f8fc17f5034cc5ee5840992
Troy Williams <maxorsomeness@gmail.com>
2017-07-02 10:47:03 +1200

    Start of alert class.

---
+++ classes/alert.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace enrol_arlo;
+
+class alert {
+    public static function create() {
+        return new alert();
+    }
+    public function send() {
+
+    }
+    public function set_subject() {
+        return $this;
+    }
+}
