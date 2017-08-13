d6aeef460c183e2cc428922817c0cf2ce8f978ed
Troy Williams <maxorsomeness@gmail.com>
2017-08-13 18:42:06 +1200

    Add validation for apiusername which is of param type email.

--- settings.php
+++ settings.php
@@ -50,7 +50,7 @@ if ($hassiteconfig) {
     $url->param('utm_campaign','Moodle plugin configuration');
     $title = get_string('opennewtabtitle', 'enrol_arlo');
     $description = get_string('apiusername_desc', 'enrol_arlo', array('url' => $url->out(), 'title' => $title));
-    $settings->add(new admin_setting_configtext('enrol_arlo/apiusername', $name, $description, null));
+    $settings->add(new admin_setting_configtext('enrol_arlo/apiusername', $name, $description, null, PARAM_EMAIL));
 
     $name = get_string('apipassword', 'enrol_arlo');
     $settings->add(new admin_setting_configpasswordunmask('enrol_arlo/apipassword', $name, '', ''));
