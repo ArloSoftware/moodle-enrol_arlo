3c1bb7b4ade886c8440de531263966b50372d434
Troy Williams <troy.williams@learningworks.co.nz>
2017-08-29 10:24:50 +1200

    Codechecker - Fix based on recommendations.

--- classes/request/collection_request.php
+++ classes/request/collection_request.php
@@ -1,4 +1,18 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace enrol_arlo\request;
 
@@ -20,7 +34,7 @@ class collection_request extends abstract_request {
         try {
             $requesturi = $this->requesturi;
             $schedule = $this->schedule;
-            /// Set default order by if empty.
+            // Set default order by if empty.
             if (empty($requesturi->getOrderBy())) {
                 $requesturi->setOrderBy('LastModifiedDateTime ASC');
             }
@@ -34,15 +48,9 @@ class collection_request extends abstract_request {
                     $date = \DateTime::createFromFormat('U', 0, $tz);
                     $latestmodified = $date->format(DATE_ISO8601);
                 }
-                $createdfilter = Filter::create()
-                    ->setResourceField('CreatedDateTime')
-                    ->setOperator('gt')
-                    ->setDateValue($latestmodified);
+                $createdfilter = Filter::create()->setResourceField('CreatedDateTime')->setOperator('gt')->setDateValue($latestmodified);
                 $requesturi->addFilter($createdfilter);
-                $modifiedfilter = Filter::create()
-                    ->setResourceField('LastModifiedDateTime')
-                    ->setOperator('gt')
-                    ->setDateValue($latestmodified);
+                $modifiedfilter = Filter::create()->setResourceField('LastModifiedDateTime')->setOperator('gt')->setDateValue($latestmodified);
                 $requesturi->addFilter($modifiedfilter);
             }
 
