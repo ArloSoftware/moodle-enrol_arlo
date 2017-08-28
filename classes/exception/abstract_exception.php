656c608aac723519f1a345ac20f71424f660edf4
Troy Williams <troy.williams@learningworks.co.nz>
2017-08-29 10:03:05 +1200

    Codechecker - Fix based on recommendations.

--- classes/exception/abstract_exception.php
+++ classes/exception/abstract_exception.php
@@ -1,4 +1,18 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace enrol_arlo\exception;
 
@@ -18,8 +32,7 @@ class abstract_exception extends \Exception {
                                 $stringidentifier = '',
                                 $parameters = array(),
                                 $apiexception = null,
-                                \Throwable $previous = null)
-    {
+                                \Throwable $previous = null) {
         $this->message = $message;
         $this->code = $code;
         $this->stringidentifier = $stringidentifier;
@@ -44,7 +57,7 @@ class abstract_exception extends \Exception {
     /**
      * @return mixed
      */
-    public function get_api_exception(){
+    public function get_api_exception() {
         return $this->apiexception;
     }
 }
