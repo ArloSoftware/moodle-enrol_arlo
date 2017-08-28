cbd545bddc944df0e09be386c504d46cf18f2885
Troy Williams <troy.williams@learningworks.co.nz>
2017-08-29 10:27:48 +1200

    Codechecker - Fix based on recommendations.

--- classes/result.php
+++ classes/result.php
@@ -1,4 +1,18 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace enrol_arlo;
 
@@ -11,6 +25,8 @@ use completion_completion;
 use grade_item;
 use enrol_arlo\Arlo\AuthAPI\Enum\RegistrationStatus;
 use enrol_arlo\Arlo\AuthAPI\Enum\RegistrationOutcome;
+
+defined('MOODLE_INTERNAL') || die();
 
 require_once("$CFG->libdir/completionlib.php");
 require_once("$CFG->libdir/gradelib.php");
