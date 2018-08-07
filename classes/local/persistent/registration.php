e6ef6d75d0f6bb2a50be96468a2e9bd77485fc39
Troy Williams <troy.williams@learningworks.co.nz>
2018-08-07 15:59:58 +1200

    Fix class namespaces.

--- classes/local/persistent/registration.php
+++ classes/local/persistent/registration.php
@@ -21,7 +21,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace enrol_arlo\local\config;
+namespace enrol_arlo\local\persistent;
 
 defined('MOODLE_INTERNAL') || die();
 
