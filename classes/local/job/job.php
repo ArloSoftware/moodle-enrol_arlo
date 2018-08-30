69cecb159cfe944d30b6d024afee190c320522ed
Troy Williams <maxorsomeness@gmail.com>
2018-08-30 20:03:05 +1200

    Add time constants and has errors method.

--- classes/local/job/job.php
+++ classes/local/job/job.php
@@ -25,33 +25,44 @@
 namespace enrol_arlo\local\job;
 
 use enrol_arlo\persistent;
+use null_progress_trace;
 
 defined('MOODLE_INTERNAL') || die();
 
 abstract class job {
 
+    /** @var TIME_PERIOD_DELAY time in seconds to delay next request. */
+    const TIME_PERIOD_DELAY = 900; // 15 Minutes.
+
+    /** @var TIME_PERIOD_EXTENSION time in seconds to extend pass no more requests time. */
+    const TIME_PERIOD_EXTENSION = 259200; // 72 Hours.
+
+    /** @var TIME_LOCK_TIMEOUT time in seconds to wait for a lock before giving up. */
+    const TIME_LOCK_TIMEOUT = 5; // 5 Seconds.
+
     protected $errors;
 
     protected $jobpersistent;
 
-    protected $processed;
+    public function __construct(persistent $jobpersistent) {
+        $this->errors = [];
+        $this->jobpersistent = $jobpersistent;
+    }
 
     public function add_error($error) {
         $this->errors[] = $error;
     }
 
-    public function __construct(persistent $jobpersistent) {
-        $this->errors = [];
-        $this->jobpersistent = $jobpersistent;
-        $this->processed = 0;
+    public function get_errors() {
+        return $this->errors;
+    }
+
+    public function has_errors() {
+        return 0 !== count($this->errors);
     }
 
     public function get_job_persistent() {
         return $this->jobpersistent;
-    }
-
-    public function get_errors() {
-        return $this->errors;
     }
 
     abstract protected function run();
