e120880f9a84bb0cb73662de29e04abd394f1aa5
Troy Williams <troy.williams@learningworks.co.nz>
2018-08-31 12:14:25 +1200

    Add processing logic for online activities job.

--- classes/local/job/online_activities_job.php
+++ classes/local/job/online_activities_job.php
@@ -25,10 +25,85 @@ namespace enrol_arlo\local\job;
 
 defined('MOODLE_INTERNAL') || die();
 
+use enrol_arlo\api;
+use enrol_arlo\Arlo\AuthAPI\RequestUri;
+use enrol_arlo\Arlo\AuthAPI\Resource\AbstractCollection;
+use enrol_arlo\local\client;
 use enrol_arlo\local\config\arlo_plugin_config;
+use enrol_arlo\local\persistent\online_activity_persistent;
+use GuzzleHttp\Psr7\Request;
+use moodle_exception;
 
 class online_activities_job extends job {
 
-    public function run() {}
+    public function run() {
+        $pluginconfig = new arlo_plugin_config();
+        $jobpersistent = $this->get_job_persistent();
+        try {
+            $hasnext = true;
+            while ($hasnext) {
+                $hasnext = false; // Break paging by default.
+                $uri = new RequestUri();
+                $uri->setHost($pluginconfig->get('platform'));
+                $uri->setResourcePath('onlineactivities/');
+                $uri->addExpand('OnlineActivity/EventTemplate');
+                $filter = "(LastModifiedDateTime gt datetime('" . $jobpersistent->get('lastsourcetimemodified') . "'))";
+                if ($jobpersistent->get('lastsourceid')) {
+                    $filter .= " OR ";
+                    $filter .= "(LastModifiedDateTime eq datetime('" . $jobpersistent->get('lastsourcetimemodified') . "')";
+                    $filter .= " AND ";
+                    $filter .= "OnlineActivityID gt " . $jobpersistent->get('lastsourceid') . ")";
+                }
+                $uri->setFilterBy($filter);
+                $uri->setOrderBy("LastModifiedDateTime ASC,OnlineActivityID ASC");
+                $request = new Request('GET', $uri->output(true));
+                $response = api::send_request(client::get_instance(), $request);
+                $collection = api::parse_response($response);
+                if ($collection instanceof AbstractCollection && $collection->count() > 0) {
+                    foreach ($collection as $resource) {
+                        $sourceid       = $resource->OnlineActivityID;
+                        $sourceguid     = $resource->UniqueIdentifier;
+                        $code           = $resource->Code;
+                        $name           = $resource->Name;
+                        $contenturi     = $resource->ContentUri;
+                        $sourcestatus   = $resource->Status;
+                        $sourcecreated  = $resource->CreatedDateTime;
+                        $sourcemodified = $resource->LastModifiedDateTime;
+                        $eventtemplate  = $resource->getEventTemplate();
+                        if ($eventtemplate) {
+                            $sourcetemplateid   = $eventtemplate->TemplateID;
+                            $sourcetemplateguid = $eventtemplate->UniqueIdentifier;
+                        }
+                        try {
+                            $onlineactivity = new online_activity_persistent();
+                            $onlineactivity->from_record_property('sourceguid', $sourceguid);
+                            $onlineactivity->set('sourceid', $sourceid);
+                            $onlineactivity->set('code', $code);
+                            $onlineactivity->set('name', $name);
+                            $onlineactivity->set('contenturi', $contenturi);
+                            $onlineactivity->set('sourcestatus', $sourcestatus);
+                            $onlineactivity->set('sourcecreated', $sourcecreated);
+                            $onlineactivity->set('sourcemodified', $sourcemodified);
+                            $onlineactivity->set('sourcetemplateid', $sourcetemplateid);
+                            $onlineactivity->set('sourcetemplateguid' , $sourcetemplateguid);
+                            $onlineactivity->save();
+                            $jobpersistent->set('lastsourceid', $sourceid);
+                            $jobpersistent->set('lastsourcetimemodified', $sourcemodified);
+                            $jobpersistent->update();
+                        } catch (moodle_exception $exception) {
+                            $this->add_error($exception->getMessage());
+                            debugging($exception->getMessage(), DEBUG_DEVELOPER, $exception->getTrace());
+                        }
+                    }
+                }
+                $hasnext = (bool) $collection->hasNext();
+            }
+        } catch (moodle_exception $exception) {
+            $this->add_error($exception->getMessage());
+            debugging($exception->getMessage(), DEBUG_DEVELOPER, $exception->getTrace());
+            return false;
+        }
+        return true;
+    }
 
 }
