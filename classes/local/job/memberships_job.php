5472bb0fc363b254d13870021ee5a182bd4a6af4
Troy Williams <troy.williams@learningworks.co.nz>
2018-09-07 17:22:37 +1200

    Added locking and fetching.

--- classes/local/job/memberships_job.php
+++ classes/local/job/memberships_job.php
@@ -38,14 +38,60 @@ use moodle_exception;
 class memberships_job extends job {
 
     public function run() {
+        $plugin = api::get_enrolment_plugin();
+        $pluginconfig = $plugin->get_plugin_config();
+        $lockfactory = static::get_lock_factory();
+        $lock = $lockfactory->get_lock($this->get_lock_resource(), self::TIME_LOCK_TIMEOUT);
         try {
-            $plugin = api::get_enrolment_plugin();
-            $config = $plugin->get_plugin_config();
             $jobpersistent = $this->get_job_persistent();
             $enrolmentinstance = $plugin::get_instance_record($jobpersistent->get('instanceid'), MUST_EXIST);
+            if ($lock) {
+                // We don't know how many records we will be retreiving it maybe 5 it maybe 5000,
+                // and the page size limit is 250. So we have to keep calling the endpoint and
+                // adjusting and the filter each call to so we get all records and don't end up
+                // getting same 250 each call.
+                $hasnext = true;
+                while ($hasnext) {
+                    $hasnext = false; // Break paging by default.
+                    $uri = new RequestUri();
+                    $uri->setHost($pluginconfig->get('platform'));
+                    $uri->setResourcePath($jobpersistent->get('endpoint'));
+                    $uri->addExpand('Registration/Contact');
+                    $resourcename = $jobpersistent->get_resource_name();
+                    $resourceidname = $resourcename . 'ID';
+                    $expand = 'Registration/' . $resourcename;
+                    $uri->addExpand($expand);
+                    $uri->setPagingTop(250);
+                    $filter = "(LastModifiedDateTime gt datetime('" . $jobpersistent->get('lastsourcetimemodified') . "'))";
+                    if ($jobpersistent->get('lastsourceid')) {
+                        $filter .= " OR ";
+                        $filter .= "(LastModifiedDateTime eq datetime('" . $jobpersistent->get('lastsourcetimemodified') . "')";
+                        $filter .= " AND ";
+                        $filter .= "RegistrationID gt " . $jobpersistent->get('lastsourceid') . ")";
+                    }
+                    $uri->setFilterBy($filter);
+                    $uri->setOrderBy("LastModifiedDateTime ASC,RegistrationID ASC");
+                    $request = new Request('GET', $uri->output(true));
+                    $response = client::get_instance()->send_request($request);
+                    $collection = api::parse_response($response);
+                    if ($collection->count() > 0) {
+                        try {
+                            foreach ($collection as $registrationresource) {
+                                $contactresource = $registrationresource->getContact();
+                            }
+                        } catch (Exception $exception) {
+                            $this->add_error($exception->getMessage());
+                        }
+                    }
+                }
+            }
         } catch (Exception $exception) {
             $this->add_error($exception->getMessage());
+            return false;
+        } finally {
+            $lock->release();
         }
+        return true;
     }
 
 }
