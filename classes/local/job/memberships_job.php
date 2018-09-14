331199ca5b8d1d51cfa0f4c3a78edc6130e0d128
Troy Williams <troy.williams@learningworks.co.nz>
2018-09-14 17:15:23 +1200

    Extract and save registration and contact details.

--- classes/local/job/memberships_job.php
+++ classes/local/job/memberships_job.php
@@ -29,8 +29,9 @@ use enrol_arlo\api;
 use enrol_arlo\Arlo\AuthAPI\RequestUri;
 use enrol_arlo\Arlo\AuthAPI\Resource\AbstractCollection;
 use enrol_arlo\local\client;
-use enrol_arlo\local\config\arlo_plugin_config;
-use enrol_arlo\local\persistent\event_persistent;
+use enrol_arlo\local\persistent\contact_persistent;
+use enrol_arlo\local\persistent\registration_persistent;
+use enrol_arlo\local\response_processor;
 use GuzzleHttp\Psr7\Request;
 use Exception;
 use moodle_exception;
@@ -41,12 +42,20 @@ class memberships_job extends job {
         $plugin = api::get_enrolment_plugin();
         $pluginconfig = $plugin->get_plugin_config();
         $lockfactory = static::get_lock_factory();
+        $trace = self::get_trace();
         try {
             $jobpersistent = $this->get_job_persistent();
             $enrolmentinstance = $plugin::get_instance_record($jobpersistent->get('instanceid'), MUST_EXIST);
             if ($enrolmentinstance->status == ENROL_INSTANCE_DISABLED) {
                 $this->add_reasons('Enrolment instance disabled.');
                 return false;
+            }
+            if (!$pluginconfig->get('allowhiddencourses')) {
+                $course = get_course($enrolmentinstance->courseid);
+                if (!$course->visible) {
+                    $this->add_reasons('Course is hidden. Allow hidden courses is not set.');
+                    return false;
+                }
             }
             $lock = $lockfactory->get_lock($this->get_lock_resource(), self::TIME_LOCK_TIMEOUT);
             if ($lock) {
@@ -79,18 +88,83 @@ class memberships_job extends job {
                     $response = client::get_instance()->send_request($request);
                     $collection = response_processor::process($response);
                     if ($collection->count() > 0) {
-                        try {
-                            foreach ($collection as $registrationresource) {
-                                $contactresource = $registrationresource->getContact();
+                        foreach ($collection as $resource) {
+                            try {
+                                /** @var $resource enrol_arlo\Arlo\AuthAPI\Resource\Registration */
+                                $sourceid = $resource->RegistrationID;
+                                $sourceguid = $resource->UniqueIdentifier;
+                                $contactresource = $resource->getContact();
+                                if (empty($contactresource)) {
+                                    throw new moodle_exception('Contact missing from Registration.');
+                                }
+                                $eventresource = $resource->getEvent();
+                                $onlineactivityresource = $resource->getOnlineActivity();
+                                if (empty($eventresource) && empty($onlineactivityresource)) {
+                                    throw new moodle_exception('Event or Online Activity missing from Registration.');
+                                }
+                                $registration = registration_persistent::get_record(
+                                    ['sourceguid' => $sourceguid]
+                                );
+                                if (!$registration) {
+                                    $registration = new registration_persistent();
+                                    $registration->set('sourceid', $sourceid);
+                                    $registration->set('sourceguid', $sourceguid);
+                                }
+                                $registration->set('enrolid', $enrolmentinstance->id);
+                                $registration->set('attendance', $resource->Attendance);
+                                $registration->set('outcome', $resource->Outcome);
+                                $registration->set('grade', $resource->Grade);
+                                $registration->set('progresspercent', $resource->ProgressPercent);
+                                $registration->set('progressstatus', $resource->ProgressStatus);
+                                $registration->set('lastactivity', $resource->LastActivityDateTime);
+                                $registration->set('sourcestatus', $resource->Status);
+                                $registration->set('sourcecreated', $resource->CreatedDateTime);
+                                $registration->set('sourcemodified', $resource->LastModifiedDateTime);
+                                $registration->set('sourcecontactid', $contactresource->ContactID);
+                                $registration->set('sourcecontactguid', $contactresource->UniqueIdentifier);
+                                if (!empty($eventresource)) {
+                                    $registration->set('sourceeventid', $eventresource->EventID);
+                                    $registration->set('sourceeventguid', $eventresource->UniqueIdentifier);
+                                }
+                                if (!empty($onlineactivityresource)) {
+                                    $registration->set('sourceonlineactivityid', $onlineactivityresource->OnlineActivityID);
+                                    $registration->set('sourceonlineactivityguid', $onlineactivityresource->UniqueIdentifier);
+                                }
+                                // Check is existing contact record.
+                                $contact = $registration->get_contact();
+                                if (!$contact) {
+                                    $contact = new contact_persistent();
+                                    $contact->set('sourceid', $contactresource->ContactID);
+                                    $contact->set('sourceguid', $contactresource->UniqueIdentifier);
+                                }
+                                $contact->set('firstname', $contactresource->FirstName);
+                                $contact->set('lastname', $contactresource->LastName);
+                                $contact->set('email', $contactresource->Email);
+                                $contact->set('codeprimary', $contactresource->CodePrimary);
+                                $contact->set('phonework', $contactresource->PhoneWork);
+                                $contact->set('phonemobile', $contactresource->PhoneMobile);
+                                $contact->set('sourcestatus', $contactresource->Status);
+                                $contact->set('sourcecreated', $contactresource->CreatedDateTime);
+                                $contact->set('sourcemodified', $contactresource->LastModifiedDateTime);
+
+                                $registration->save();
+                                $contact->save();
+
+                            } catch (moodle_exception $exception) {
+                                $this->add_error($exception->getMessage());
+                                debugging($exception->getMessage(), DEBUG_DEVELOPER); print_object($exception);
+                                throw $exception; // DEBUG just re throwing for testing. Need to log to record?.
                             }
-                        } catch (Exception $exception) {
-                            $this->add_error($exception->getMessage());
                         }
+                        $hasnext = (bool) $collection->hasNext();
                     }
                 }
+            } else {
+                throw new moodle_exception('locktimeout');
             }
         } catch (Exception $exception) {
             $this->add_error($exception->getMessage());
+            debugging($exception->getMessage(), DEBUG_DEVELOPER);
             return false;
         } finally {
             $lock->release();
