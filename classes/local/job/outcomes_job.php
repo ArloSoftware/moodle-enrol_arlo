f83f7958c2d19dd46568429a27cd27a347c9524d
Troy Williams <troy.williams@learningworks.co.nz>
2018-09-20 11:40:34 +1200

    Save any error to registration persistent, add allowoutcomespushing, add PHPDocs

--- classes/local/job/outcomes_job.php
+++ classes/local/job/outcomes_job.php
@@ -15,6 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * Job class for sending outcome data to individual registrations.
  *
  * @package   enrol_arlo {@link https://docs.moodle.org/dev/Frankenstyle}
  * @copyright 2018 LearningWorks Ltd {@link http://www.learningworks.co.nz}
@@ -27,31 +28,39 @@ defined('MOODLE_INTERNAL') || die();
 
 use enrol_arlo\api;
 use enrol_arlo\Arlo\AuthAPI\RequestUri;
-use enrol_arlo\Arlo\AuthAPI\Resource\AbstractCollection;
 use enrol_arlo\local\client;
-use enrol_arlo\local\config\arlo_plugin_config;
-use enrol_arlo\local\persistent\event_persistent;
 use enrol_arlo\local\persistent\registration_persistent;
 use enrol_arlo\result;
 use GuzzleHttp\Psr7\Request;
-use Exception;
 use moodle_exception;
 
+/**
+ * Job class for sending outcome data to individual registrations.
+ *
+ * @package   enrol_arlo {@link https://docs.moodle.org/dev/Frankenstyle}
+ * @copyright 2018 LearningWorks Ltd {@link http://www.learningworks.co.nz}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class outcomes_job extends job {
+
     public function run() {
         $trace = self::get_trace();
         $plugin = api::get_enrolment_plugin();
         $pluginconfig = $plugin->get_plugin_config();
+        if (!$pluginconfig->get('allowoutcomespushing')) {
+            $this->add_reasons(get_string('outcomespushingdisabled', 'enrol_arlo'));
+            return false;
+        }
         $lockfactory = static::get_lock_factory();
         $lock = $lockfactory->get_lock($this->get_lock_resource(), self::TIME_LOCK_TIMEOUT);
         if ($lock) {
+            $jobpersistent = $this->get_job_persistent();
             try {
-                $jobpersistent = $this->get_job_persistent();
                 $enrolmentinstance = $plugin::get_instance_record($jobpersistent->get('instanceid'));
                 if (!$enrolmentinstance) {
                     $jobpersistent->set('disabled', 1);
                     $jobpersistent->save();
-                    throw new moodle_exception('No matching enrolment instance');
+                    throw new moodle_exception(get_string('nomatchingenrolmentinstance', 'enrol_arlo'));
                 }
                 if ($enrolmentinstance->status == ENROL_INSTANCE_DISABLED) {
                     $jobpersistent->set('timelastrequest', time());
@@ -70,11 +79,11 @@ class outcomes_job extends job {
                 }
                 $registrations = registration_persistent::get_records(['enrolid' => $enrolmentinstance->id]);
                 if ($registrations) {
-                    foreach ($registrations as $registration) {
-                        $result = new result($enrolmentinstance->courseid, $registration->to_record());
+                    foreach ($registrations as $registrationpersistent) {
+                        $result = new result($enrolmentinstance->courseid, $registrationpersistent->to_record());
                         $uri = new RequestUri();
                         $uri->setHost($pluginconfig->get('platform'));
-                        $endpoint = $jobpersistent->get('endpoint') . $registration->get('id') .'/';
+                        $endpoint = $jobpersistent->get('endpoint') . $registrationpersistent->get('id') .'/';
                         $uri->setResourcePath($endpoint);
                         $request = new Request(
                             'PATCH',
@@ -82,13 +91,24 @@ class outcomes_job extends job {
                             ['Content-type' => 'application/xml; charset=utf-8'],
                             $result->export_to_xml());
                         $response = client::get_instance()->send_request($request);
+                        // Failed, likely to be 400 or 409. TODO pull in values from new request.
+                        if ($response->getStatusCode() != 200) {
+                            $jobpersistent->add_error($response->getReasonPhrase());
+                            $registrationpersistent->add_error_message($response->getReasonPhrase());
+                            $registrationpersistent->save();
+                            // Don't want to break whole job, so moving along.
+                            continue;
+                        }
+                        // We need to set changed properties back on registration record. This is
+                        // important as required when determining the use of add or replace in the
+                        // patch request.
                         $changed = $result->get_changed();
                         foreach (get_object_vars($changed) as $field => $value) {
-                            $registration->set($field, $value);
+                            $registrationpersistent->set($field, $value);
                         }
-                        $registration->set('updatesource', 0);
-                        $registration->save();
-
+                        // Reset update flag.
+                        $registrationpersistent->set('updatesource', 0);
+                        $registrationpersistent->save();
                         // Update scheduling information on persistent after successfull save.
                         $jobpersistent->set('timelastrequest', time());
                         $jobpersistent->save();
@@ -96,11 +116,11 @@ class outcomes_job extends job {
                 }
                 return true;
             } catch (moodle_exception $exception) {
+                debugging($exception->getMessage(), DEBUG_DEVELOPER);
+                $this->add_error($exception->getMessage());
                 if ($exception instanceof invalid_persistent_exception || $exception instanceof coding_exception) {
                     throw $exception;
                 }
-                debugging($exception->getMessage(), DEBUG_DEVELOPER);
-                $this->add_error($exception->getMessage());
                 return false;
             } finally {
                 $lock->release();
