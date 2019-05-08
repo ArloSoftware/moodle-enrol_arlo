c2c2f637ccb031dbd3a35117109416beaf50a54d
Troy Williams <troy.williams@learningworks.co.nz>
2019-05-08 16:00:22 +1200

    Set minimum PHP requirements to PHP 7.1

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInite4aad6fbfefb391c58bfe8300aa4260b::getLoader();
+return ComposerAutoloaderInitd09ed8f9bb72f3038f5add3cc7a65356::getLoader();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,13 +4,13 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInite4aad6fbfefb391c58bfe8300aa4260b
+class ComposerStaticInitd09ed8f9bb72f3038f5add3cc7a65356
 {
     public static $files = array (
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
         '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
-        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
+        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
     );
 
@@ -72,8 +72,8 @@ class ComposerStaticInite4aad6fbfefb391c58bfe8300aa4260b
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInite4aad6fbfefb391c58bfe8300aa4260b::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInite4aad6fbfefb391c58bfe8300aa4260b::$prefixDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitd09ed8f9bb72f3038f5add3cc7a65356::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitd09ed8f9bb72f3038f5add3cc7a65356::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
