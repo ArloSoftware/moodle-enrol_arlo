046664ca5df7d6dd40689cced1033f0e6a1b9891
Troy Williams <troy.williams@learningworks.co.nz>
2019-10-22 10:23:44 +1300

    Add ManageUri functionality.

---
+++ classes/Arlo/AuthAPI/Enum/IntegrationData.php
@@ -0,0 +1,8 @@
+<?php namespace enrol_arlo\Arlo\AuthAPI\Enum;
+
+class IntegrationData {
+
+    /** @var string VendorID used to identify this integration. */
+    const VendorID = 'ArloMoodlePlugin';
+
+}
