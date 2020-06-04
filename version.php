a76bd98f9a5c6e142f3d7972ce4d2183e5af3d84
Troy Williams <troy.williams@learningworks.co.nz>
2020-06-05 11:15:25 +1200

    Update version and release.

--- version.php
+++ version.php
@@ -27,9 +27,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin            = new stdClass();
-$plugin->version   = 2020060300;
+$plugin->version   = 2020060500;
 $plugin->requires  = 2017111300;        // See http://docs.moodle.org/dev/Moodle_Version
 $plugin->component = 'enrol_arlo';      // Full name of the plugin (used for diagnostics).
-$plugin->release   = '3.8.2';           // Human-friendly version name.
+$plugin->release   = '3.8.3';           // Human-friendly version name.
 $plugin->maturity  = MATURITY_STABLE;   // This version's maturity level.
 $plugin->dependencies = [];
