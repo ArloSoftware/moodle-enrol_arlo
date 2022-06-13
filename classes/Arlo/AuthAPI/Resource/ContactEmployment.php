86212beb01cd8fa73286899b464b52fe04423e2f
Donald Barrett <one.donaldb@live.com>
2022-06-13 18:29:27 +1200

    Add resource for ContactEmployment
    Class to represent a Contacts employment resource

---
+++ classes/Arlo/AuthAPI/Resource/ContactEmployment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace enrol_arlo\Arlo\AuthAPI\Resource;
+
+/**
+ * Class ContactEmployment
+ * @package enrol_arlo\Arlo\AuthAPI\Resource
+ */
+class ContactEmployment extends AbstractResource {
+    public $Organisation;
+}
