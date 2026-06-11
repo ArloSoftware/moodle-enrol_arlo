dac445dab6f4d84b94b48c860c1ac1e8b82a6f18
Oscar Nadjar <oscar.nadjar@moodle.com>
2026-06-11 07:36:03 +0200

    Bumping version

--- version.php
+++ version.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin            = new stdClass();
 
-$plugin->version   = 2026021900;
+$plugin->version   = 2026061100;
 $plugin->requires  = 2022112800;        // See http://docs.moodle.org/dev/Moodle_Version
 $plugin->component = 'enrol_arlo';      // Full name of the plugin (used for diagnostics).
 $plugin->release   = '5.2.0';       // Human-friendly version name.
